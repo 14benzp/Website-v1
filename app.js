ce25042d17fbdd5234b5f817807446bbcda5e18a
Phil B <14benzp@gmail.com>
2025-02-12 07:17:33 -0500

    created light and dark theme button to change theme of site

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+const themeswither = document.querySelector('.btn');
+
+themeswither.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme"){
+        this.textContent = "Dark";
+
+    } else {
+        this.textContent = "Light";
+    }
+
+    console.log('current class name: ' + className);
+});
